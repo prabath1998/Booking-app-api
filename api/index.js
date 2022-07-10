47da675a0fb3fb1fbf3f45fb09447dd28dccd76a
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-10 23:37:37 +0530

    create hotel rest endpoint added

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import authRoute from "./routes/auth.js"
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 const app = express();
 dotenv.config();
@@ -24,7 +27,12 @@ mongoose.connection.on("connected",()=>{
 });
 
 //middlewares
-app.use("/auth",authRoute)
+app.use(express.json());
+
+app.use("/api/auth",authRoute);
+app.use("/api/users",usersRoute);
+app.use("/api/hotels",hotelsRoute);
+app.use("/api/rooms",roomsRoute);
 
 app.listen(8800, () => {
     connect();
