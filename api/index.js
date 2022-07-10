97879d181c8cf5f4bce21096625bc3a3dd6c884f
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-10 22:52:16 +0530

    configure mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,28 @@
 import express from "express";
-const app = express();
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-app.listen(8800,()=>{
-    console.log("Connected to backend!");
-})
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongodb");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected",()=>{
+    console.log("mongoDB disconnected");
+});
+
+mongoose.connection.on("connected",()=>{
+    console.log("mongoDB connected");
+});
+
+app.listen(8800, () => {
+    connect();
+  console.log("Connected to backend!");
+});
