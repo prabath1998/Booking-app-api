a9a673f34d50b880be8a3b827b6d38528b627308
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-11 09:55:03 +0530

    error handling

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
 
 const router = express.Router();
 
@@ -40,23 +41,22 @@ router.delete("/:id", async (req, res) => {
 
 //GET BY ID
 router.get("/:id", async (req, res) => {
-    try {
-      const hotel = await Hotel.findById(req.params.id);
-      res.status(200).json(hotel);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-  //GET ALL
-  router.get("/", async (req, res,next) => {
-    try {
-      const hotels = await Hotel.find();
-      res.status(200).json(hotels);
-    } catch (error) {
-      // res.status(500).json(error);
-      next(error);
-    }
-  });
+//GET ALL
+router.get("/", async (req, res, next) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
