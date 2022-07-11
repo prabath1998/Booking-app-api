13799a6f8fa9893100112f3cfc56609ad0b43b4b
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-11 13:57:15 +0530

    verifyuser,verifyadmin authentications added

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,12 +5,16 @@ import {
   getUsers,
   updateUser,
 } from "../controllers/userController.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/checkauthentication", verifyToken, (req, res) => {
     res.send("hello user, you are logged in!")
+});
+
+router.get("/checkuser/:id",verifyUser, (req, res, next) => {
+    res.send("hello user, you are logged in  & you can delete your account!")
 });
 
 //UPDATE
