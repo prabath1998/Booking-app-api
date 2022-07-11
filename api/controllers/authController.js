a29a586b0c08ccd3517a98e31a383ea912a0a1f7
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-11 11:54:57 +0530

    encrypt password & bug fixed

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,16 +1,21 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res, next) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
 
     await newUser.save();
     res.status(200).send("User has been registered successfully");
   } catch (error) {
     next(error);
+    
   }
 };
