d1346c024a920d91543b001535c7662325ef29bc
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-11 14:12:14 +0530

    changes in authentication

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,17 +5,23 @@ import {
   getUsers,
   updateUser,
 } from "../controllers/userController.js";
-import { verifyToken, verifyUser } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req, res) => {
-    res.send("hello user, you are logged in!")
-});
+// router.get("/checkauthentication", verifyToken, (req, res) => {
+//     res.send("hello user, you are logged in!")
+// });
 
-router.get("/checkuser/:id",verifyUser, (req, res, next) => {
-    res.send("hello user, you are logged in  & you can delete your account!")
-});
+// router.get("/checkuser/:id",verifyUser, (req, res, next) => {
+//     res.send("hello user, you are logged in  & you can delete your account!")
+// });
+
+
+
+// router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+//   res.send("hello admin, you are logged in and you can delete all accounts")
+// })
 
 //UPDATE
 router.put("/:id", updateUser);
